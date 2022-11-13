2be2b775d65d30212064210f9f3a14a5c0213772
Dominika Papánová <dominika.papanova2@gmail.com>
2022-11-13 12:57:17 +0100

    gallery update

--- App/Views/Gallery/index.view.php
+++ App/Views/Gallery/index.view.php
@@ -7,6 +7,7 @@
                     <div class="card shadow-sm"  >
                         <img src="./public/images/svokryne.webp" alt="product image">
                         <div class="card-body">
+                            <h5 clas="card-title">Sansevieria trifasciata ‘Laurentii’</h5>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             <div class="d-flex justify-content-between align-items-center">
 
@@ -16,111 +17,6 @@
                     </div>
                 </div>
 
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <img src="./public/images/ficus.webp" alt="product image">
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-
-                                <small class="text-muted">10.29€</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <img src="./public/images/voskovka.webp" alt="product image">
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-
-                                <small class="text-muted">15.99€</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <img src="./public/images/adsondii.webp" alt="product image">
-
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-
-                                <small class="text-muted">32.59€</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <img src="./public/images/calathea.webp" alt="product image">
-
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-
-                                <small class="text-muted">13.29€</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <img src="./public/images/alocasia.webp" alt="product image">
-
-
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-
-                                <small class="text-muted">13.99€</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <img src="./public/images/begonia.webp" alt="product image">
-
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-
-                                <small class="text-muted">9.99€</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <img src="./public/images/palma.webp" alt="product image">
-
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-
-                                <small class="text-muted">12.99€</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card shadow-sm">
-                        <img src="./public/images/ornata.webp" alt="product image">
-
-                        <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-
-                                <small class="text-muted">20.99€</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <?php foreach ($data as $row) { ?>
                 <div class="col">
                     <div class="card shadow-sm">
