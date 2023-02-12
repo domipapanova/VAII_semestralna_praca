c573115fa03d10e21bbf697f16f397fc3d6fbbde
Dominika Papánová <dominika.papanova2@gmail.com>
2023-02-12 18:25:47 +0100

    blog validation done

--- App/Controllers/BlogController.php
+++ App/Controllers/BlogController.php
@@ -33,42 +33,43 @@ class BlogController extends AControllerBase
         $article = ($id ? Article::getOne($id) : new Article());
 
         if(isset($data['title']) && isset($data['text'])) {
-            if (isset($_FILES['picture'])) {
-                $article->setTitle($this->request()->getValue('title'));
-                $article->setText($this->request()->getValue('text'));
-                /* if ($this->request()->getValue('obrazok') != null) {
+            if(strlen($data['title']) < 255 && strlen($data['text'])< 2000 ) {
+                if (isset($_FILES['article_picture'])) {
+                    $article->setTitle($this->request()->getValue('title'));
+                    $article->setText($this->request()->getValue('text'));
 
-                     $article->setPictureName($this->request()->getValue('obrazok'));
-                 }*/
-                if (isset($data['link'])) {
-                    $article->setLinks($this->request()->getValue('link'));
+                    if (isset($data['link'])) {
+                        if (filter_var($data['link'], FILTER_VALIDATE_URL)) {
+                            $article->setLinks($this->request()->getValue('link'));
+                        }
+                    }
+
+                    $errors = [];
+                    $file_name = $_FILES['article_picture']['name'];
+                    $file_size = $_FILES['article_picture']['size'];
+                    $file_tmp = $_FILES['article_picture']['tmp_name'];
+                    $file_type = $_FILES['article_picture']['type'];
+                    $file_info = pathinfo($_FILES['article_picture']['name']);
+                    $file_ext = strtolower($file_info['extension']);
+
+                    $extensions = ["jpeg", "jpg", "png", "webp"];
+                    if (in_array($file_ext, $extensions) === false) {
+                        $errors[] = "Nesprávny formát, povolené sú JPEG alebo PNG";
+                    }
+
+                    if ($file_size > 2097152) {
+                        $errors[] = 'Veľkosť obrázka musí byť menšia než 2 MB';
+                    }
+
+                    if (empty($errors)) {
+                        move_uploaded_file($file_tmp, "./public/images/" . $file_name);
+                        // Store the path to the image in your database
+                        // $img_path = "./public/images/" . $file_name;
+                        $article->setPictureName($file_name);
+                    }
+
+                    $article->save();
                 }
-
-                $errors = [];
-                $file_name = $_FILES['picture']['name'];
-                $file_size = $_FILES['picture']['size'];
-                $file_tmp = $_FILES['picture']['tmp_name'];
-                $file_type = $_FILES['picture']['type'];
-                $file_info = pathinfo($_FILES['picture']['name']);
-                $file_ext = strtolower($file_info['extension']);
-
-                $extensions = ["jpeg", "jpg", "png", "webp"];
-                if (in_array($file_ext, $extensions) === false) {
-                    $errors[] = "Nesprávny formát, povolené sú JPEG alebo PNG";
-                }
-
-                if ($file_size > 2097152) {
-                    $errors[] = 'Veľkosť obrázka musí byť menšia než 2 MB';
-                }
-
-                if (empty($errors)) {
-                    move_uploaded_file($file_tmp, "./public/images/" . $file_name);
-                    // Store the path to the image in your database
-                    // $img_path = "./public/images/" . $file_name;
-                    $article->setPictureName($file_name);
-                }
-
-                $article->save();
             }
 
             return $this->redirect("?c=blog");
--- App/Views/Blog/create.view.php
+++ App/Views/Blog/create.view.php
@@ -7,6 +7,8 @@ use App\Models\Article;
 $article = $data['article'];
 ?>
 <link rel="stylesheet" href="public/css/blogStyle.css">
+<script src="public/js/blog_script.js"></script>
+
 
 <div class="article-form">
 <form name="newArticle" method="post" action="?c=blog&a=store" enctype="multipart/form-data" onsubmit="return validateArticle()">
@@ -15,19 +17,19 @@ $article = $data['article'];
     <div class="form-group">
         <label for="exampleFormControlInput1">Titulok</label>
         <p id="title_input" hidden></p>
-        <input type="text"  maxlength="255" class="form-control" id="exampleFormControlInput1" name="title" value="<?=$article->getTitle();?>" required>
+        <input type="text"  maxlength="255" class="form-control" id="title" name="title" value="<?=$article->getTitle();?>" required>
     </div>
 
     <div class="form-group">
         <label for="exampleFormControlTextarea1">Popis článku</label>
         <p id="text_input" hidden></p>
-        <input  type="text" maxlength="2000" class="form-control" id="exampleFormControlTextarea1" rows="3" name="text" value="<?=$article->getText();?>" required></input>
+        <input  type="text" maxlength="2000" class="form-control" id="text" rows="3" name="text" value="<?=$article->getText();?>" required></input>
     </div>
 
     <div class="form-group">
         <label for="exampleFormControlInput1">Odkaz na článok</label>
         <p id="link_input" hidden></p>
-        <input type="url" maxlength="2048" class="form-control" id="exampleFormControlInput1" name="link" value="<?=$article->getLinks();?>">
+        <input type="url" maxlength="2048" class="form-control" id="link" name="link" value="<?=$article->getLinks();?>">
     </div>
 
     <div class="form-group">
